fca18f16bef4d70d342183e06d04d409b4cb4844
howardmann <howardmann27@gmail.com>
2016-12-02 21:54:12 +1100

    chanmged knex file

--- knexfile.js
+++ knexfile.js
@@ -13,8 +13,12 @@ module.exports = {
 
   production: {
     client: 'pg',
-    ssl: true,
-    connection: process.env.DATABASE_URL
+    connection: {
+      host: 'ec2-54-163-254-48.compute-1.amazonaws.com',
+      user: 'fdmaefajoouwpq',
+      password: 'stTfCZ3neUD9Pr9gtC01LgLX93',
+      database: '5432/damc31c8bhoc65'
+    }
   }
 
 };
