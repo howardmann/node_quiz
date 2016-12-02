e39db538efe3282e3e90a666ca354d0f808c4fd4
howardmann <howardmann27@gmail.com>
2016-12-02 22:17:20 +1100

    rever back

--- knexfile.js
+++ knexfile.js
@@ -13,13 +13,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: 'ec2-54-163-254-48.compute-1.amazonaws.com',
-      user: 'fdmaefajoouwpq',
-      password: 'stTfCZ3neUD9Pr9gtC01LgLX93',
-      database: 'damc31c8bhoc65',
-      ssl: true
-    }
+    connection: process.env.DATABASE_URL
   }
 
 };
