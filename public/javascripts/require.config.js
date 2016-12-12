a395f583d7acb309c73efa93e1b228c4b2b797dd
howardmann <howardmann27@gmail.com>
2016-12-12 21:03:35 +1100

    Added cdn load

--- public/javascripts/require.config.js
+++ public/javascripts/require.config.js
@@ -1,11 +1,11 @@
 require.config({
   baseUrl: '/javascripts',
   paths: {
-    jquery: './lib/jquery',
-    backbone: './lib/backbone',
-    underscore: './lib/underscore',
-    handlebars: './lib/handlebars',
-    text: './lib/text'
+    jquery: ['https://code.jquery.com/jquery-3.1.0.min','./lib/jquery'],
+    backbone: ['https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min','./lib/backbone'],
+    underscore: ['https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min','./lib/underscore'],
+    handlebars: ['https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.6/handlebars.min','./lib/handlebars'],
+    text: ['https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min','./lib/text']
   },
   shim: {
     backbone: {
